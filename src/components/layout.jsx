663b252f6e8761b2bf1476701c395c4ccdcabec5
kirre02 <kirre02@outlook.com>
2025-01-14 09:33:30 +0100

    fix: add children to layout component

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,12 +1,13 @@
 import Header from "./header";
 
-function Layout() {
+function Layout({children}) {
   return (
     <body className="antialiased bg-[--bg-color] text-white font-mono tracking-normal leading-normal flex flex-col min-h-screen selection:bg-sky-50 selection:text-black">
       <div className="flex flex-col flex-grow">
         <Header />
 
         <main className="mt-10 flex flex-col gap-3 px-5 pt-5 flex-grow">
+          {children}
         </main>
       </div>
     </body>
